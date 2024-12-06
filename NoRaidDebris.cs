8fb400af1da02c788b2d1d61810eaecbfb6adc9e
VisEntities <visentities@gmail.com>
2024-12-06 17:33:42 +0100

    Add project files.

---
+++ NoRaidDebris.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Copyright (C) 2024 Game4Freak.io
+ * This mod is provided under the Game4Freak EULA.
+ * Full legal terms can be found at https://game4freak.io/eula/
+ */
+
+namespace Oxide.Plugins
+{
+    [Info("No Raid Debris", "VisEntities", "1.0.0")]
+    [Description("Disables debris that blocks building during raids.")]
+    public class NoRaidDebris : RustPlugin
+    {
+        #region Fields
+
+        private static NoRaidDebris _plugin;
+
+        #endregion Fields
+
+        #region Oxide Hooks
+
+        private void Init()
+        {
+            _plugin = this;
+        }
+
+        private void Unload()
+        {
+            _plugin = null;
+        }
+
+        private void OnEntitySpawned(DebrisEntity debrisEntity)
+        {
+            if (debrisEntity != null)
+                debrisEntity.RemoveCorpse();
+        }
+
+        #endregion Oxide Hooks
+    }
+}
